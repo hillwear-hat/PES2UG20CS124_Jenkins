da15f50f3cfe3e3b76f5313c20853353c1cb8c14
giridhara maanas <76812021+hillwear-hat@users.noreply.github.com>
2023-02-17 19:27:29 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "yo   i made a chage in the code which was asked in task4 \n";
+  cout << "yo I made a chage in the code which was asked in task4 \n";
   return 0;
 }
